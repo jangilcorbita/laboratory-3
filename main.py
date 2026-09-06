51610f8472a2288c602ad62899995629c4550fa6
Nicole Bernadette A. Monsanto <nbamonsanto@mymail.mapua.edu.ph>
2026-09-06 15:24:37 +0800

    Add order.py and integrate OrderManager into main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from customer import CustomerManager
 from menu import Menu
+from order import OrderManager
 
 class FoodDeliverySystem:
     def __init__(self):
         self.customer_manager = CustomerManager()
         self.menu_manager = Menu()
+        self.order_manager = OrderManager()
 
     def run(self):
         while True:
@@ -25,7 +27,7 @@ class FoodDeliverySystem:
             elif choice == '2': 
                 self.menu_manager.view_menu()
             elif choice == '3': 
-                ...
+                self.order_manager.create_order(self.customer_manager, self.menu_manager)
             elif choice == '4':
                 ...
             elif choice == '5':
---
+++ order.py
@@ -0,0 +1,39 @@
+from models import Order
+
+class OrderManager:
+    def __init__(self):
+        self.orders = {}
+        self.order_counter = 1
+
+    def create_order(self, customer_manager, menu_manager):
+        print("\n")
+        print("Create New Order")
+        
+        customer_id = input("Enter Customer ID: ")
+        if customer_id not in customer_manager.customers:
+            print("Customer not found! Please register first.")
+            return
+
+        order_items = []
+        while True:
+            item_id = input("Enter Menu Item ID (or 'done' to finish): ").upper()
+            if item_id == 'DONE':
+                break
+            
+            if item_id in menu_manager.menu:
+                order_items.append(menu_manager.menu[item_id])
+                print(f"Added: {menu_manager.menu[item_id].name}")
+            else:
+                print("Item ID not found in menu.")
+
+        if not order_items:
+            print("Order cannot be empty.")
+            return
+
+        order_id = f"ORDER#{self.order_counter:03d}"
+        new_order = Order(order_id, customer_id, order_items)
+        self.orders[order_id] = new_order
+        self.order_counter += 1
+
+        total = sum(item.price for item in order_items)
+        print(f"\nOrder {order_id} created successfully! Total: ₱{total:.2f}")
